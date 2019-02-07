8ab9083f2ffd6a6e5c62e409f5ed1a0f2e7dae6e
rashmy <rashmy@RASHMY-PC2>
2019-02-07 12:05:22 -0800

    changes

--- source/code/plugin/filter_health_cpu_memory.rb
+++ source/code/plugin/filter_health_cpu_memory.rb
@@ -17,10 +17,12 @@ module Fluent
         
         @@previousCpuHealthDetails = {"State": "", "Time": "", "Percentage": ""}
         @@previousPreviousCpuHealthDetails = {"State": "", "Time": "", "Percentage": ""}
-        #@@currentCpuHealthState = ''
+        @@currentHealthMetrics = {}
+
         #@@lastEmittedCpuHealthState = ''
         @@previousMemoryRssHealthDetails = {"State": "", "Time": "", "Percentage": ""}
         @@previousPreviousMemoryRssHealthDetails = {"State": "", "Time": "", "Percentage": ""}
+        @@currentHealthMetrics = {}
         @@clusterName = KubernetesApiClient.getClusterName
         @@clusterId = KubernetesApiClient.getClusterId
         @@clusterRegion = KubernetesApiClient.getClusterRegion
@@ -112,19 +114,83 @@ module Fluent
             end
         end
 
+        def processHealthMetrics()
+            healthRecord = {}
+            currentCpuHealthDetails = {}
+            cpuMetricPercentValue = @@currentHealthMetrics['cpuUsageNanoCoresPercentage']
+            cpuMetricValue = @@currentHealthMetrics['cpuUsageNanoCores']
+            memoryRssMetricPercentValue = @@currentHealthMetrics['memoryRssBytesPercentage']
+            memoryRssMetricValue = @@currentHealthMetrics['memoryRssBytes']
+            updateHealthState = false
+            if cpuMetricValue.to_f < 80.0
+                #nodeCpuHealthState = 'Pass'
+                cpuHealthState = "Pass"
+             elsif cpuMetricValue.to_f > 90.0
+                cpuHealthState = "Fail"
+             else
+                cpuHealthState = "Warning"
+             end
+             currentCpuHealthDetails['State'] = cpuHealthState
+             currentCpuHealthDetails['Time'] = @@currentHealthMetrics['metricTime']
+             currentCpuHealthDetails['CPUUtilPercentage'] = cpuMetricPercentValue
+             currentCpuHealthDetails['CPUUsageMillicores'] = cpuMetricValue
+
+             if (cpuHealthState == @@previousCpuHealthDetails['State']) && (cpuHealthState == @@previousPreviousCpuHealthDetails['State'])
+                healthRecord['NodeCpuHealthState'] = cpuHealthState
+                healthRecord['NodeCpuUtilizationPercentage'] = cpuMetricPercentValue
+                healthRecord['NodeCpuMilliCores'] = cpuMetricValue
+                #healthRecord['TimeStateDetected'] = @@previousPreviousCpuHealthDetails['Time']
+                healthRecord['CollectionTime'] = @@previousPreviousCpuHealthDetails['Time']
+                healthRecord['PrevNodeCpuUtilizationDetails'] = { "Percent": @@previousCpuHealthDetails["Percentage"], "TimeStamp": @@previousCpuHealthDetails["Time"], "Millicores": @@previousCpuHealthDetails['CPUUsageMillicores']}
+                healthRecord['PrevPrevNodeCpuUtilizationDetails'] = { "Percent": @@previousPreviousCpuHealthDetails["Percentage"], "TimeStamp": @@previousPreviousCpuHealthDetails["Time"], "Millicores": @@previousPreviousCpuHealthDetails['CPUUsageMillicores']}
+                updateHealthState = true
+            end
+            @@previousPreviousCpuHealthDetails['State'] = @@previousCpuHealthDetails['State']
+            @@previousPreviousCpuHealthDetails['Time'] = @@previousCpuHealthDetails['Time']
+            @@previousCpuHealthDetails['State'] = healthState
+            @@previousCpuHealthDetails['Time'] = metricTime
+            @@previousCpuHealthDetails = healthState
+
+
+        end
+
+        def filter(tag, time, record)
+            # Reading all the records to populate a hash for CPU and memory utilization percentages and values
+            #metricRecord = {}
+            #metricRecord[record['data']['baseData']['metric']] = record['data']['baseData']['series'][0]['min']
+            @@currentHealthMetrics[record['data']['baseData']['metric']] = record['data']['baseData']['series'][0]['min']
+            if !(@@currentHealthMetrics.has_key?("metricTime"))
+                @@currentHealthMetrics['metricTime'] = record['time']
+            end
+            if !(@@currentHealthMetrics.has_key?("computer"))
+                @@currentHealthMetrics['computer'] = record['data']['baseData']['series'][0]['dimValues'][0]
+            end
+            #@@currentHealthMetrics[record['data']['baseData']['metric']] = record['data']['baseData']['series'][0]['min']
+            #@@currentHealthMetrics
+            return nil
+        end
+
         def filter_stream(tag, es)
             health_es = MultiEventStream.new
-            es.each { |time, record|
-              begin
-                filtered_record = filter(tag, time, record)
-                #filtered_records.each {|filtered_record| 
-                    health_es.add(time, filtered_record) if filtered_record
-                    router.emit_stream('oms.rashmi', health_es) if health_es
-                #} if filtered_records
-              rescue => e
+            #currentHealthMetrics = {}
+            begin
+                es.each { |time, record|
+                #begin
+                    #filteredRecord = filter(tag, time, record)
+                    #currentHealthMetrics['TimeStamp'] = 
+                    filter(tag, time, record)
+                    #@@currentHealthMetrics.merge!(filteredRecord)
+                    #currentHealthMetrics[filteredRecord.keys.first.to_s] = filtered_record[filteredRecord.keys.first.to_s]
+                    #TODO: Optimize this to read these values only from first record
+                    #processHealthMerics(currentHealthMetrics)
+                    #health_es.add(time, filtered_record) if filtered_record
+                    #router.emit_stream('oms.rashmi', health_es) if health_es
+                #end  
+                }
+                processHealthMetrics
+            rescue => e
                 router.emit_error_event(tag, time, record, e)
-              end
-            }
+            end
             es
         end
 
